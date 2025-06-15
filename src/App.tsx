ae00b804560b8ddbc70e90f31453759036f4b0d3
Akash Patel <akashpatel.profile@gmail.com>
2025-06-15 19:18:24 +0530

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import { Box, Button, TextField, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
-import api from './api/api.jsx'
+import api from './api/api'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [name, setName] = useState('')
-  const [pass, setPass] = useState('')
-  const [data, setData] = useState()
-
-  const checkLoginStatus = () => {
-    const token = localStorage.getItem('user')
-    if (token) {
-      // api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-      setIsLoggedIn(true)
-      fetchData()
-    }
-  }
-
-  const fetchData = async () => {
-    api
-      .get('/protected')
-      .then((res) => console.log(res.data))
-      .catch((err) => console.error(err))
-  }
 
   const handleLogin = async () => {
     api
@@ -34,55 +15,33 @@ function App() {
       .catch((err) => console.error(err))
   }
 
-  const handleLogout = () => {
-    localStorage.removeItem('user')
-    setIsLoggedIn(false)
+  const getData = () => {
+    api
+      .get('/protected')
+      .then((res) => {
+        console.log(res.data)
+        setIsLoggedIn(true)
+      })
+      .catch((err) => {
+        console.error(err)
+        if (err.status === 401) {
+          setIsLoggedIn(false)
+        }
+      })
   }
 
   useEffect(() => {
-    checkLoginStatus()
+    getData()
   }, [])
 
   return (
     <Box sx={{ width: '24rem', margin: '2rem auto' }}>
       {isLoggedIn ? (
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          {data ? (
-            <>
-              <Typography>{data}</Typography>
-              <Button onClick={handleLogout}>Logout</Button>
-            </>
-          ) : (
-            <>
-              <Typography>
-                Some Error Occurred. Please refresh and login
-              </Typography>
-            </>
-          )}
-        </Box>
+        <Typography>Logged In</Typography>
       ) : (
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            marginTop: '1rem',
-            gap: '1rem',
-          }}
-        >
-          <TextField
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <TextField
-            name="pass"
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
-          />
-          <Button variant="contained" onClick={handleLogin}>
-            Login
-          </Button>
-        </Box>
+        <Button variant="contained" onClick={handleLogin}>
+          Login
+        </Button>
       )}
     </Box>
   )
--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   baseURL: 'https://cookie-token-server.onrender.com:8080/',
+  // baseURL: 'http://localhost:8080/',
   timeout: 1000,
   withCredentials: true,
 })
